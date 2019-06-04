78b8e34e7cffa68274cf2e37d38314ed83d3787a
junho <junho@funlab.kr>
2019-06-04 16:18:08 +0900

    style change

--- angular-tour-of-heroes/src/app/app.component.ts
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
+/**
+ * 스타일은 여러개 파일 지정 가능. 같은 셀렉터일 경우 마지막 css 파일의 스타일 적용
+ */
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  //styleUrls: ['./app.component.css'],
-  styleUrls: ['./style.css']
+  styleUrls: ['./app.component.css','./style.css']
 })
 export class AppComponent {
   title = 'Tour of Heroes';
