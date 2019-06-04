85f75a4e32f041191685ed5d5ca3fa7fa6ef40f8
junho <junho@funlab.kr>
2019-06-04 15:06:58 +0900

    hero tutorial

---
+++ angular-tour-of-heroes/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  //styleUrls: ['./app.component.css'],
+  styleUrls: ['./style.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
---
+++ angular-tour-of-heroes/src/app/mock-heroes.ts
@@ -0,0 +1,19 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 1, name: 'IronMan'},
+    {id: 2, name: 'Captain America'},
+    {id: 3, name: 'Thor'},
+    {id: 4, name: 'Hulk'},
+    {id: 5, name: 'Vision'},
+    {id: 6, name: 'Dr.strange'},
+    {id: 7, name: 'Black Widow'},
+    {id: 8, name: 'Black Panther'},
+    {id: 9, name: 'Loki'},
+    {id: 10, name: 'Star Load'},
+    {id: 11, name: 'Gamora'},
+    {id: 12, name: 'Baby Groot'},
+    {id: 13, name: '너굴맨'},
+    {id: 14, name: 'Ant Man'},
+
+];
